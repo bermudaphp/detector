1010c4dbdeb1b4883c77ca97937c01c65d791793
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-24 16:15:43 +0200

    Create Application.php

---
+++ src/MimeTypes/Application.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bermuda\Detector\MimeTypes;
+
+final class Application
+{ 
+    public const atomXml     = 'application/atom+xml';
+    public const EDIX12      = 'application/EDI-X12';
+    public const EDIFACT     = 'application/EDIFACT';
+    public const json        = 'application/json';
+    public const javaScript  = 'application/javascript';
+    public const octetStream = 'application/octet-stream';
+}
