40bba98ec6490fcd944f37a1b4d14e9665805450
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-24 16:16:37 +0200

    Create Image.php

---
+++ src/MimeTypes/Image.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bermuda\Detector\MimeTypes;
+
+final class Image
+{
+    public const gif                = 'image/gif';
+    public const jpeg               = 'image/jpeg';
+    public const pjpeg              = 'image/pjpeg';
+    public const png                = 'image/png';
+    public const svg                = 'image/svg+xml';
+    public const tiff               = 'image/tiff';
+    public const vnd_microsoft_icon = 'image/vnd.microsoft.icon';
+    public const vnd_wap_wbmp       = 'image/vnd.wap.wbmp';
+    public const webp               = 'image/webp';
+}
