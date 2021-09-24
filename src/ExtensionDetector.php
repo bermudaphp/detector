bb52d13316b4328f4df88a6401ce462c539d817d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-24 19:06:34 +0200

    Update ExtensionDetector.php

--- src/ExtensionDetector.php
+++ src/ExtensionDetector.php
@@ -5,4 +5,5 @@ namespace Bermuda\Detector;
 interface ExtensionDetector
 {
     public function detectExtension(string $content): string ;
+    public function detectFileExtension(string $filename): string ;
 }
