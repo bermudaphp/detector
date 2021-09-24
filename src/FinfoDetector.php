5a48ec5b9b4d36dcd35c4fcdc25da628a89626dd
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-24 19:05:03 +0200

    Update FinfoDetector.php

--- src/FinfoDetector.php
+++ src/FinfoDetector.php
@@ -2,16 +2,52 @@
 
 namespace Bermuda\Detector;
 
+use finfo;
+use Devanych\Mime\MimeTypes;
+
 final class FinfoDetector implements ContentDetector
 {
+    /**
+     * @param string $filename
+     * @return string
+     */
+    public function detectFileMimeType(string $filename): string
+    {
+        return $this->detectMimeType(file_get_contents($filename));
+    }
+
+    /**
+     * @param string $content
+     * @return string
+     */
     public function detectMimeType(string $content): string
     {
         return $this->finfoBuffer(FILEINFO_MIME_TYPE, $content);
     }
+    
+    /**
+     * @param string $filename
+     * @return string
+     */
+    public function detectFileExtension(string $filename): string
+    {
+        return $this->detectExtension(file_get_contents($filename));
+    }
 
+    /**
+     * @param string $content
+     * @return string
+     */
     public function detectExtension(string $content): string
     {
-        return $this->finfoBuffer(FILEINFO_EXTENSION, $content);
+        $ext = $this->finfoBuffer(FILEINFO_EXTENSION, $content);
+
+        if ($ext === '???') {
+            $mimeType = $this->detectMimeType($content);
+            return (new MimeTypes)->getExtensions($mimeType);
+        }
+
+        return $ext;
     }
 
     /**
@@ -21,6 +57,6 @@ final class FinfoDetector implements ContentDetector
      */
     private function finfoBuffer(string $mode, string $content): string
     {
-        return (new \finfo($mode))->buffer($content);
+        return (new finfo($mode))->buffer($content);
     }
 }
