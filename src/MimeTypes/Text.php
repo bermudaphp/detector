ad468a40d48d54a4700e4a286cddaa250487bb61
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-24 16:17:17 +0200

    Create Text.php

---
+++ src/MimeTypes/Text.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bermuda\Detector\MimeTypes;
+
+final class Text
+{
+    public const html               = 'text/html';
+    public const plain              = 'text/plain';
+    public const css                = 'text/css';
+    public const csv                = 'text/csv';
+    public const javaScript         = 'text/javascript';
+    public const php                = 'text/php';
+    public const xml                = 'text/xml';
+    public const markdown           = 'text/markdown';
+    public const cacheManifest      = 'text/cache-manifest';
+    public const cmd                = 'text/cmd';
+}
